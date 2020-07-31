72ba3d41e1232b70475a60c49059fbe7704b680c
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-07-30 23:25:44 -0300

    📕  (node) filtering, pagination and sorting

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,7 +1,29 @@
 const info = () => `This is the API of a Hackernews Clone`;
 
-const feed = (parent, args, context) =>
-  context.prisma.link.findMany();
+const feed = async (parent, args, context) => {
+  const where = args.filter
+    ? {
+      OR: [
+        { description: { contains: args.filter } },
+        { url: { contains: args.filter } },
+      ],
+    }
+    : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+
+  const count = await context.prisma.link.count({ where });
+
+  return {
+    links,
+    count,
+  };
+}
 
 const link = (parent, args, context) =>
   context.prisma.link.findOne({ where: { id: Number(args.linkId) } });
